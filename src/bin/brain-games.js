f10595253008297f3058140b74333b10abd0c4b6
RuslanHam <rurham@yandex.ru>
2018-02-12 19:32:32 +0400

    ver 0.1.2 welcome UserName

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,3 +1,7 @@
 #! /usr/bin/env node
 
-console.log("Welcome to the Brain Games!");
+import hello from '../index.js';
+
+console.log("Welcome to the Brain Games!\n");
+hello();
+//require('src/index.js');
