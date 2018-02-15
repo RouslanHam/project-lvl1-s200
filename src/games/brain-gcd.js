dfa5139be8e81b0fb99f4a0721e75200157711e5
RuslanHam <rurham@yandex.ru>
2018-02-15 15:24:47 +0400

    maxDivisor

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -6,14 +6,14 @@ const diapasonRand = 20;
 const beginRand = 1;
 
 const tasks = () => {
-  const nod = (n1, n2) => {
+  const maxDivisor = (n1, n2) => {
     if (n2 === 0) return n1;
-    return nod(Math.min(n1, n2), Math.abs(n1 - n2));
+    return maxDivisor(Math.min(n1, n2), Math.abs(n1 - n2));
   };
   const num1 = random(diapasonRand, beginRand);
   const num2 = random(diapasonRand, beginRand);
   const quiz = `${num1} ${num2}`;
-  const answerCorrect = nod(num1, num2);
+  const answerCorrect = maxDivisor(num1, num2);
   const result = [quiz, answerCorrect];
   return result;
 };
